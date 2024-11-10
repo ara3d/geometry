dc14a9ef6ff56cfd4de504edff8c5f3a42ecef2a
Christopher Diggins <cdiggins@gmail.com>
2024-11-09 19:53:37 -0500

    Project ocmpiles again, need to run some tests.

--- Ara3D.Geometry/Modifiers.cs
+++ Ara3D.Geometry/Modifiers.cs
@@ -37,37 +37,5 @@ namespace Ara3D.Geometry
                 ? mesh.Deform(v => (v * MathUtil.Inverse(snapSize)).Truncate() * snapSize)
                 : mesh.Deform(v => Vector3.Zero);
 
-        //==
-
-        public static Vector3 Skew(Vector3 v, Line line, Vector3 from, Vector3 to)
-            => throw new NotImplementedException();
-
-        public static Vector3 Taper(Vector3 v)
-            => throw new NotImplementedException();
-
-        public static Vector3 Bend(Vector3 v)
-            => throw new NotFiniteNumberException();
-
-        public static Vector3 Twist(Vector3 v)
-            => throw new NotImplementedException();
-
-        public static Line Axis(this AABox box, Axis axis)
-            => throw new NotImplementedException();
-
-        public static Vector3 Translate(Vector3 v, Vector3 amount)
-            => throw new NotImplementedException();
-
-        public static Func<Vector3, Vector3> ApplyFallOff(Func<Vector3, Vector3> f, Func<Vector3, float> fallOff)
-            => throw new NotImplementedException();
-        
-        public static Func<Vector3, float> AmountAlongLine(Vector3 v, Line line)
-            => throw new NotImplementedException();
-
-        public static Func<Vector3, float> AmountAlongAxis(Vector3 v, AABox box, Axis axis)
-            => throw new NotImplementedException();
-
-        public static Func<Vector3, float> DistanceAsAmount(Vector3 v, Vector3 center, float max)
-            => throw new NotImplementedException();
-
     }
 }
